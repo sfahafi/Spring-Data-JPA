8df171451605865494159202c46024c63db58086
sfahafi <sfahafi18@gmail.com>
2022-01-15 18:48:16 -0300

    Convirtiendo la aplicacion web a consola con Interfaz CommandLineRunner. Ajuste DB y Repo

--- src/main/java/com/sfahafi/JpaDemoApplication.java
+++ src/main/java/com/sfahafi/JpaDemoApplication.java
@@ -1,13 +1,32 @@
 package com.sfahafi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaDemoApplication {
+public class JpaDemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Ejemplos de Spring Data JPA");
+		guardar();
+		eliminar();
+		
+	}
+	
+	
+	private void guardar() {
+		System.out.println("Insertando un registro");
+	}
+	
+	private void eliminar() {
+		System.out.println("Eliminando un registro");
+	}
+	
 
 }
