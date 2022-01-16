560bd94f766cfb02c1c769b41b8e2c1275603d10
sfahafi <sfahafi18@gmail.com>
2022-01-15 22:13:36 -0300

    Uso de metodos crudRepository

--- src/main/java/com/sfahafi/JpaDemoApplication.java
+++ src/main/java/com/sfahafi/JpaDemoApplication.java
@@ -1,32 +1,105 @@
 package com.sfahafi;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sfahafi.model.Categoria;
+import com.sfahafi.repository.I_CategoriasRepository;
+
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private I_CategoriasRepository cr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
+	
 
 	@Override
-	public void run(String... args) throws Exception {
-		//System.out.println("Ejemplos de Spring Data JPA");
-		guardar();
-		eliminar();
+	public void run(String... args) throws Exception {  // psvm seria como la clase principal ejecutable en una aplicacion de escritorio
+		guardarTodas();	
 		
 	}
 	
+	private void guardarTodas() {
+		 
+	}
 	
-	private void guardar() {
-		System.out.println("Insertando un registro");
+	private void existeId() {
+		boolean existe = cr.existsById(5);
+		System.out.println("Existe: " + existe);
+	}
+	
+	private void buscarTodo() {
+		Iterable<Categoria> categorias = cr.findAll();
+		for (Categoria cat : categorias) {
+			System.out.println(cat);
+			
+		}
+	}
+	
+	private void encontrarPorIds() {
+		List<Integer> ids = new LinkedList<Integer>();
+		ids.add(1);
+		ids.add(4);
+		ids.add(10);
+		Iterable<Categoria> categorias = cr.findAllById(ids);
+		for (Categoria cat : categorias) {
+			System.out.println(cat);
+			
+		}
+	}
+	
+	private void eliminarTodo() {
+		cr.deleteAll();
+	}
+	
+	private void conteo() {
+		long count = cr.count();
+		System.out.println("Total categorias: " + count);
 	}
 	
 	private void eliminar() {
-		System.out.println("Eliminando un registro");
+		int idCategoria = 1;
+		cr.deleteById(idCategoria);
 	}
 	
+	private void modificar() {
+		Optional<Categoria> optional = cr.findById(2);
+		if(optional.isPresent()) {
+			Categoria catTem = optional.get();
+			catTem.setNombre("Electronica");
+			cr.save(catTem);
+			System.out.println(optional.get());
+		}else 
+			System.out.println("Categoria no encontrada");
+	}
+	
+	private void buscarPorId() {
+		Optional<Categoria> optional = cr.findById(1);
+		if(optional.isPresent())
+			System.out.println(optional.get());
+		else 
+			System.out.println("Categoria no encontrada");
+	}
+	
+	private void guardar() {
+		Categoria cat = new Categoria();
+		cat.setNombre("Finanzas");
+		cat.setDescripcion("Trabajos relacionados con contabilidad");
+		cr.save(cat);
+		System.out.println(cat);
+	}
+	
+		
 
 }
---
+++ src/main/java/com/sfahafi/repository/I_CategoriasRepository.java
@@ -0,0 +1,9 @@
+package com.sfahafi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sfahafi.model.Categoria;
+
+public interface I_CategoriasRepository extends CrudRepository<Categoria, Integer> {
+
+}
